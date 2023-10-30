2763a998ee692115b384b00bf7be7c9b93806e5a
COR Coding <corcoding@CJCheatamsiPad2.localdomain>
2023-10-30 14:27:22 -0400

    refactored app.jsx again for organization, added an attempt tracker to the game  and a useContext to update the attempts

---
+++ src/components/AnswerAttempts.jsx
@@ -0,0 +1,18 @@
+import React, { useContext, useState } from "react";
+import { PlayerAttemptsContext } from "../App";
+import "../styles/AnswerAttempts.css";
+
+const AnswerAttempts = () => {
+  const {attempts} = useContext(PlayerAttemptsContext)
+  return (
+    <div className="answer-attempts-container">
+      <div className="attempts">
+        <h2>Attempts - {attempts}</h2>
+        {/* <p className="attempts-number">{attempts}</p> */}
+
+      </div>
+    </div>
+  );
+};
+
+export default AnswerAttempts;
