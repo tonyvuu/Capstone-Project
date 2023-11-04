4a9c6d9ca061dcc87a008e54ab2388e9aa2fb284
Tony Vu <tony.vuu18@gmail.com>
2023-11-03 22:11:46 -0400

    finished all the steps

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -12,7 +12,7 @@ const Step1 = () => {
   const [showForm, setShowForm] = useState(false);
   const { updateProgress } = useContext(CompletionBarContext);
 
-  const text = `DevTown, GA - At 4:23am this morning, a man broke into a remote beach house where 26-year-old twin Toni and MaKenna Doe were asleep. The unidentified man murdered MaKenna and abducted Toni before first responders could arrive. Police departments across two states are hunting for MaKenna's killer and her sister Toni, but neither has been found. Homicide detective, Manti Fettuccine, made a breakthrough in the case but then mysteriously disappeared. He left his detective's notebook behind but authorities aren't sure what to make of it. The killer is on the loose.\n\nCASE FACTS: VICTIM: Makenna Doe\nCASE NUMBER: G-668H\nLOCATION: DevTown, GA`;
+  const text = `DevTown, GA - At 4:23am this morning, a man broke into a remote beach house where 26-year-old twins, Toni and MaKenna Doe, were asleep. The unidentified man murdered MaKenna and abducted Toni before first responders could arrive. Police departments across two states are hunting for MaKenna's killer and her sister Toni, but neither have been found. Homicide detective, Morgan Wayne, made a breakthrough in the case but then mysteriously disappeared. He left his detective's notebook behind but authorities weren't sure what to make of it. The killer is on the loose.\n\nCASE FACTS: VICTIM: Makenna Doe\nCASE NUMBER: G-668H\nLOCATION: DevTown, GA`;
 
   const speed = 100;
 
