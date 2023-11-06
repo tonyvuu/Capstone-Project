ce7056f1b3ba744519a59f3df1b001d6194b35de
Santiago Felizzola <santi@EVI-LabHP-7.localdomain>
2023-11-06 13:44:21 -0500

    added password edge casing, cleaned up code

--- src/headercomponents/Registration.jsx
+++ src/headercomponents/Registration.jsx
@@ -4,7 +4,6 @@ import Form from "react-bootstrap/Form";
 import { useNavigate, Link } from "react-router-dom";
 import "../styles/Registration.css";
 
-
 const Registration = () => {
   const [newUser, setNewUser] = useState({
     firstName: "",
@@ -22,83 +21,96 @@ const Registration = () => {
   const navigate = useNavigate();
 
   const { firstName, lastName, email, password, reenterpassword } = newUser;
+  const minLength = 6;
+  const maxLength = 20;
 
   const handleRegister = async (e) => {
     e.preventDefault();
 
     if (reenterpassword !== password) {
       const passwordInput = document.getElementById("formBasicPassword");
-      const reenterpasswordInput = document.getElementById("formBasicReenterPassword");
+      const reenterpasswordInput = document.getElementById(
+        "formBasicReenterPassword"
+      );
       const passwordLabel = document.getElementById("password-label");
-      const reenterpasswordLabel = document.getElementById("reenterpassword-label");
+      const reenterpasswordLabel = document.getElementById(
+        "reenterpassword-label"
+      );
 
       setPasswordError("Passwords do not match");
       passwordInput.style.border = "1px solid red";
       reenterpasswordInput.style.border = "1px solid red";
+      setTimeout(() => {
+        setPasswordError("");
+        passwordInput.style.border = "1px solid #555";
+        reenterpasswordInput.style.border = "1px solid #555";
+        passwordLabel.style.color = "#ff6a00"
+        reenterpasswordLabel.style.color = "#ff6a00"
+      }, 2500);
       passwordLabel.style.color = "red";
       reenterpasswordLabel.style.color = "red";
-      return;
-    }
-
-    if (firstName.length < 2 || firstName.length > 30) {
+    } else if (firstName.length < 2 || firstName.length > 30) {
       setFirstNameError("First name should be between 2 and 30 characters");
-      return;
-    } else {
-      setFirstNameError("");
-    }
-
-    if (!/^[A-Za-z]+$/.test(firstName)) {
+      setTimeout(() => {
+        setFirstNameError("");
+      }, 2500);
+    } else if (!/^[A-Za-z]+$/.test(firstName)) {
       setFirstNameError("First name should only contain letters");
-      return;
-    } else {
-      setFirstNameError("");
-    }
-
-    if (lastName.length < 2 || lastName.length > 30) {
+      setTimeout(() => {
+        setFirstNameError("");
+      }, 2500);
+    } else if (lastName.length < 2 || lastName.length > 30) {
       setLastNameError("Last name should be between 2 and 30 characters");
-      return;
-    } else {
-      setLastNameError("");
-    }
-
-    if (!/^[A-Za-z]+$/.test(lastName)) {
+      setTimeout(() => {
+        setLastNameError("");
+      }, 2500);
+    } else if (!/^[A-Za-z]+$/.test(lastName)) {
       setLastNameError("Last name should only contain letters");
-      return;
-    } else {
-      setLastNameError("");
-    }
-    if (email.trim() === "") {
+      setTimeout(() => {
+        setLastNameError("");
+      }, 2500);
+    } else if (email.trim() === "") {
       setEmailError("Email address cannot be empty");
-      return;
+      setTimeout(() => {
+        setEmailError("");
+      }, 2500);
+    } else if (password.trim() === "") {
+      setPasswordError("Password cannot be blank");
+      setTimeout(() => {
+        setPasswordError("");
+      }, 2500);
+    } else if (password.length < minLength || password.length > maxLength ) {
+      setPasswordError("Password should be between 6 and 20 characters");
+      setTimeout(() => {
+        setPasswordError("");
+      }, 2500);
     } else {
-      setEmailError("");
-    }
-
-    const response = await fetch("http://localhost:3000/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newUser),
-    });
-
-    if (!response.ok) {
-      setEmailError("Email already exists");
-      const emailInput = document.getElementById("formBasicEmail");
-      const emailLabel = document.getElementById("email-label");
-      emailInput.style.borderColor = "red";
-      emailLabel.style.color = "red";
-
-      console.error("Fetch error:", response.status, response.statusText);
-    } else {
-      setNewUser({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        reenterpassword: "",
+      const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
       });
-      navigate("/login");
+
+      if (!response.ok) {
+        setEmailError("Email already exists");
+        const emailInput = document.getElementById("formBasicEmail");
+        const emailLabel = document.getElementById("email-label");
+        emailInput.style.borderColor = "red";
+        emailLabel.style.color = "red";
+
+        console.error("Fetch error:", response.status, response.statusText);
+      } else {
+        setNewUser({
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          reenterpassword: "",
+        });
+        navigate("/login");
+      }
     }
   };
 
@@ -152,7 +164,6 @@ const Registration = () => {
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label id="password-label">Password</Form.Label>
             <Form.Control
-              minLength={6}
               type="password"
               name="password"
               value={password}
@@ -161,9 +172,10 @@ const Registration = () => {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicReenterPassword">
-            <Form.Label id="reenterpassword-label">Re-enter password</Form.Label>
+            <Form.Label id="reenterpassword-label">
+              Re-enter password
+            </Form.Label>
             <Form.Control
-              minLength={6}
               type="password"
               name="reenterpassword"
               value={reenterpassword}
